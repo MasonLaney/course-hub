9e04def37cd212ae528ecf7b2ab80265dd1b0dd0
Pierre-Nicolas Perrin <pnp@jgp.net>
2020-10-17 23:44:31 -0400

    start day boxes

--- src/Course Hub Content/DayBox.js
+++ src/Course Hub Content/DayBox.js
@@ -2,8 +2,6 @@ import React from "react"
 
 const DayBox = (props) => {
     const classes = props.day
-    return(
-        {props.dayArr.map(item => {
-            return(<p>{item}</p>)
-        })})
+    return(<p>Hello</p>)
 }
+export default DayBox
--- src/Course Hub Content/Home.js
+++ src/Course Hub Content/Home.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import app from "../Authentication/base";
+import DayBox from "./DayBox";
 import data from "./test_deadlines.json"
 
 class Home extends Component {
@@ -18,34 +19,34 @@ class Home extends Component {
 
     categorizeClasses() {
         this.state.schedule.forEach(item => {
-
+            var newStateArray;
             //if the class is on monday
             if(item.time.includes("Mo")) {
-                var newStateArray = this.state.monday;
+                newStateArray = this.state.monday;
                 newStateArray.push(item.class)
                 this.setState({monday: newStateArray})
             }
             //if the class is on tuesday
             if(item.time.includes("Tu")) {
-                var newStateArray = this.state.tuesday;
+                newStateArray = this.state.tuesday;
                 newStateArray.push(item.class)
                 this.setState({tuesday: newStateArray})
             }
             //if the class is on wednesday
             if(item.time.includes("We")) {
-                var newStateArray = this.state.wednesday;
+                newStateArray = this.state.wednesday;
                 newStateArray.push(item.class)
                 this.setState({wednesday: newStateArray})
             }
             //if the class is on thursday
             if(item.time.includes("Th")) {
-                var newStateArray = this.state.thursday;
+                newStateArray = this.state.thursday;
                 newStateArray.push(item.class)
                 this.setState({thursday: newStateArray})
             }
             //if the class is on friday
             if(item.time.includes("Fr")) {
-                var newStateArray = this.state.friday;
+                newStateArray = this.state.friday;
                 newStateArray.push(item.class)
                 this.setState({friday: newStateArray})            }
         })
@@ -79,7 +80,7 @@ class Home extends Component {
                             <div>Class {item.class} and time {item.time}</div>
                     )})}
                 </div>
-                <DayBox
+                <DayBox dayArr={this.state.monday}/>
                 <button onClick={() => app.auth().signOut()}>Sign out</button>
             </div>
         )
