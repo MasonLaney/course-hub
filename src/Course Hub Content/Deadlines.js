b1483558737ae4a6e803466b805a1e2a14dd9da1
Pierre-Nicolas Perrin <pnp@jgp.net>
2020-10-18 11:08:13 -0400

    sort dates

--- src/Course Hub Content/Deadlines.js
+++ src/Course Hub Content/Deadlines.js
@@ -12,10 +12,14 @@ class Deadlines extends Component {
 
     //fetch schedule data when mounting
     componentDidMount() {
+        let todayDate = new Date();
+        var date = parseInt(todayDate.getMonth() + 1);
+
         this.setState()
         data.map(item => {
             var newStateArray = this.state.deadlines;
-            if(item.submitted.normalize() === "Not Started") {
+            if(item.submitted.normalize() === "Not Started"
+                && (parseInt(item.due.substring(5,7)) >= date)) {
                 newStateArray.push({
                     submitted: item.submitted,
                     title: item.title,
@@ -29,8 +33,35 @@ class Deadlines extends Component {
         })
     }
 
+    sortDeadlines() {
+        var temp1 = [];
+        var temp2 = [];
+        var temp3 = [];
+
+        //sort state by date
+        for(let i = 0; i < this.state.length-1; i++) {
+            for(let j = 0; j < this.state.length-i-1; j++) {
+                if(this.state[j].due === "No due date") {
+                    temp3 = this.state[j]
+                    this.state.splice(j)
+                    this.state = [this.state, temp3]
+                }
+                temp1 = parseInt(this.state[j].due.substring(0,9))
+                temp2 = parseInt(this.state[j+1].due.substring(0,9))
+
+                if(temp1>temp2) {
+                    temp3 = this.state[j];
+                    this.state[j] = this.state[j+1]
+                    this.state[j] = temp3
+                }
+            }
+        }
+    }
+
+
     render() {
         console.log(this.state.deadlines)
+        this.sortDeadlines()
         return(
             <div>
                 <h2 className="header">Deadlines:</h2>
