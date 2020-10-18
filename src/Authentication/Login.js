54921aca5a73ef17d11aa45e4fe286e9e7b5a0f0
MasonLaney <40879250+MasonLaney@users.noreply.github.com>
2020-10-17 23:45:37 -0400

    Added CSS styling to Login page

--- src/Authentication/Login.js
+++ src/Authentication/Login.js
@@ -4,45 +4,46 @@ import app from "./base.js";
 import { AuthContext } from "./Auth.js";
 
 const Login = ({ history }) => {
-  const handleLogin = useCallback(
-    async event => {
-      event.preventDefault();
-      const { email, password } = event.target.elements;
-      try {
-        await app
-          .auth()
-          .signInWithEmailAndPassword(email.value, password.value);
-        history.push("/");
-        
-      } catch (error) {
-        alert(error);
-      }
-    },
-    [history]
-  );
+    const handleLogin = useCallback(
+        async event => {
+	        event.preventDefault();
+	        const { email, password } = event.target.elements;
+	        try {
+	            await app
+	            .auth()
+	            .signInWithEmailAndPassword(email.value, password.value);
+	            history.push("/");
+	        } catch (error) {
+	            alert(error);
+	        }
+        },
+        [history]
+    );
 
-  const { currentUser } = useContext(AuthContext);
+    const { currentUser } = useContext(AuthContext);
 
-  if (currentUser) {
-    return <Redirect to="/" />;
-  }
+	if (currentUser) {
+        return <Redirect to="/" />;
+	}
 
-  return (
-    <div>
-      <h1>Log in</h1>
-      <form onSubmit={handleLogin}>
-        <label>
-          Onyen
-          <input name="email" type="email" placeholder="Onyen" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" placeholder="Onyen password" />
-        </label>
-        <button type="submit">Log in</button>
-      </form>
-    </div>
-  );
+    return (
+  	    <div className = "login">
+	        <h1 className="titleText">Log in to sync classes with Sakai!</h1>
+	        <p className="flavorText">(We don't store any of your info)</p>
+	        <form onSubmit={handleLogin} className="inputText">
+	            <label>
+	                Onyen:
+		            <input name="email" type="email" placeholder="Onyen" />
+	            </label>
+		        <p></p>
+	            <label>
+	                Password:
+	                <input name="password" type="password" placeholder="Onyen password" />
+	            </label>
+	            <button type="submit" className="button">Log in</button>
+	        </form>
+  	    </div>
+    );
 };
 
 export default withRouter(Login);
